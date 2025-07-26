1e3ff4a0d41e4cf5dae85ad0a333c56dce3d0c5e
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 17:56:05 +0600

    another way of assign multiple variables

--- day04/index.py
+++ day04/index.py
@@ -3,6 +3,7 @@ print("variables name are define with multi words")
 
 
 #assign single variable
+print("This is single variable")
 firstVar = "this is variables"
 print(firstVar)
 
@@ -11,6 +12,19 @@ print(firstVar)
 
 a, b, c = "MD", "NAYEEM", "SARKER"
 
+print("This is multiple variable")
 print(a)
 print(b)
 print(c)
+
+
+#another way of assign multiple variables
+
+varName = ["MD", "NAYEEM", "SARKER"]
+
+a2, b2, c2 = varName
+
+print("This is another way to assign multiple variable")
+print(a2)
+print(b2)
+print(c2)
