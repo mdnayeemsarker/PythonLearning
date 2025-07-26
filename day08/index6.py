512f8520e7e2106a8ec0f5d09d99725a68136f8c
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 21:27:36 +0600

    modify string escape charecters hex Value

--- day08/index6.py
+++ day08/index6.py
@@ -25,5 +25,11 @@ myName = "My name is MD NAYEEM SARKER,\b \b I\'m 130 years 'old'"
 print(myName)
 
 #Octal value
+#A backslash followed by three integers will result in a octal
 txt = "\110\145\154\154\157"
 print(txt)
+
+#Hex value
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
