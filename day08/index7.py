aece19fbe02c0cde863e68ff28fe09078cee233d
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 21:37:47 +0600

    python string methiod - center

--- day08/index7.py
+++ day08/index7.py
@@ -7,3 +7,8 @@ print(myName.capitalize())
 
 #casefold = Converts string into lower case
 print(myName.casefold())
+
+#center = Returns a centered string
+tet = "SARKER"
+x = tet.center(20)
+print(x)
