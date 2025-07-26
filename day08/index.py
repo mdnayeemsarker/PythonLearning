6c690e945b84ebefa58f2bf44a8a6536dd999113
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 19:48:09 +0600

    python string Quotes Inside Quotes

--- day08/index.py
+++ day08/index.py
@@ -3,3 +3,6 @@
 print("Nayeem") # double or single quote are represent the same 
 print('Nayeem') # double or single quote are represent the same 
 
+#Quotes Inside Quotes
+print("My name is 'Nayeem'")
+print('My name is "Nayeem"')
