b8f04cf759d7f33cc3fe6ca6fc8e50cf2095181f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 18:49:10 +0600

    global variable function local variable override globale variable

---
+++ day04/index4.py
@@ -0,0 +1,10 @@
+#this is the variable Syntax
+print("global variables")
+
+globaleVar = "this is an globale variable"
+
+def main():
+    globaleVar = "functiona local varable"
+    print(globaleVar)
+
+main()
