66ff71ec4f7576dfffbb4907200ce0d1302be26a
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 05:11:14 +0600

    python boolean condition if statement

--- day09/index.py
+++ day09/index.py
@@ -5,3 +5,12 @@
 print(10 > 9)
 print(10 == 9)
 print(10 < 9)
+
+#run a condition in an if statement, Python returns True or False:
+a = 255
+b = 50
+print("\n")
+if a > b :
+    print("a is bigger than b")
+else:
+    print("b is bigger than b")
