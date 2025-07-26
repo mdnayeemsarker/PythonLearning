be3f0845c29247cf10aaab2e2e470ff4524b5906
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 19:08:58 +0600

    all data types for python

---
+++ day05/index.py
@@ -0,0 +1,19 @@
+# data type
+# myVar = "this is str that means text" # this is str
+# myVar = 20 # this is integer
+# myVar = 20.5 # this is float
+# myVar = 1j # this is complex
+# myVar = ["apple", "banana", "cherry"] # this is list
+# myVar = ("apple", "banana", "cherry") # this is tuple
+# myVar = range(10) # this is range must be number
+# myVar = {"name" : "John", "age" : 36} # this is dict
+# myVar = {"apple", "banana", "cherry"} # this is set
+# myVar = frozenset({"apple", "banana", "cherry"}) # this is frozenset
+# myVar = True # this is bool
+# myVar = b"Hello" # this is bytes
+# myVar = bytearray(5) # this is bytearray
+# myVar = memoryview(bytes(5)) # this is memoryview
+myVar = None # this is NoneType
+
+print(myVar)
+print(type(myVar))
