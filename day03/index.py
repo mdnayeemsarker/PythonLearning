3cf89f2c47feb423baa220d1a3c1175eb2a4965c
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 17:45:19 +0600

    another way comment of python

--- day03/index.py
+++ day03/index.py
@@ -2,3 +2,11 @@
 print("this is comment of pyhion it make with #")
 # if 5 > 2:
   # print("Five is greater than two!")
+
+"""
+This is a comment
+written in
+more than just one line
+this way to comment moee line at a time
+"""
+print("Hello, World!")
