c382a46a0a0f4599a40efe74655a902a29c16c3a
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-26 20:45:12 +0600

    modify f-string look like placeholder and modfiers

--- day08/index5.py
+++ day08/index5.py
@@ -8,5 +8,5 @@ aboutMe = myName + myAge
 print(aboutMe)
 
 #F-String
-updateAbout = f"Update with f-String: MD NAYEEM SARKER & Age is {myAge}"
+updateAbout = f"Update with f-String: MD NAYEEM SARKER & Age is {myAge}" # here f is modfiers
 print(updateAbout)
