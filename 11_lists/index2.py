1dfc01dc18dbd7c78b45f37da1ba180228ee5d9e
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 21:48:20 +0600

    Python List Access Range of indexes

--- 11_lists/index2.py
+++ 11_lists/index2.py
@@ -11,3 +11,8 @@ print(mylist[4])
 #Negative Indexing
 print("\nNegative Indexing")
 print(mylist[-2]) # negative indexing means right to left access
+
+
+#Range of Indexes
+print("\nRange of Indexes")
+print(mylist[2:5]) 
