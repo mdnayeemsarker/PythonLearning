10140b8e0a4ac373393b0c9e874116c60d094de3
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 04:43:51 +0600

    Python - Sort Lists Sort List Numerically

--- 11_lists/index8.py
+++ 11_lists/index8.py
@@ -5,3 +5,10 @@ print("\nSort List Alphanumerically")
 myalist = ["apple", "banana", "cherry", "1apple", "2banana", "3cherry"]
 myalist.sort()
 print(myalist)
+
+
+#Sort List Numerically
+print("\nSort the list numerically")
+mynlist = [100, 50, 65, 82, 23]
+mynlist.sort()
+print(mynlist)
