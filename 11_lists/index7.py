32b310862955db3f49b9c9d3d2e163d863efd07b
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 04:39:04 +0600

    Python - List Comprehension

---
+++ 11_lists/index7.py
@@ -0,0 +1,11 @@
+#Python - List Comprehension
+
+#List Comprehension
+print("\nList Comprehension")
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+print(newlist)
