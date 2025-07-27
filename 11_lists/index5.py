34b5853431aa5902d210d899e1d977df99c0bcec
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 01:46:54 +0600

    Python - Remove List Items Remove Specified Item

---
+++ 11_lists/index5.py
@@ -0,0 +1,8 @@
+#Python - Remove List Items Remove Specified Item
+
+#Remove Specified Item
+print("\nAppend Items")
+
+mylist = ["apple", "banana", "cherry"]
+mylist.remove("banana")
+print(mylist)
