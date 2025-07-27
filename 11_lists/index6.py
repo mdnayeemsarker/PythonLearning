1867b2e6cb25de53c185a39bab2b3e18de351fba
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 04:33:58 +0600

    Python - Loop Through the Index Numbers

--- 11_lists/index6.py
+++ 11_lists/index6.py
@@ -7,3 +7,8 @@ mylist = ["apple", "banana", "cherry", "apple", "banana", "cherry"]
 
 for x in mylist:
   print(x)
+
+
+#Loop Through the Index Numbers
+for i in range(len(mylist)):
+  print(mylist[i])
