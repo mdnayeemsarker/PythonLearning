51c68ca3e6b6c89e9109448b706ed2edd0befccd
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 14:36:25 +0600

    Python Assignment Operators

--- day10/index.py
+++ day10/index.py
@@ -11,4 +11,60 @@ print(15%6) #Modulus
 print(2 ** 6) #Exponentiation
 print(25 // 2) #Floor division = the floor division // rounds the result down to the nearest whole number
 
+#Assignment Operators are used to assign values to variables:
+print("\nAssignment Operators")
 
+a = 25
+b = 12
+
+print(a)
+
+c = 6
+c += a
+print(c)
+
+c = 36
+c -= a
+print(c)
+
+c = 3
+c *= a
+print(c)
+
+c = 100
+c /= a
+print(c)
+
+c = 160
+c %= a
+print(c)
+
+c = 160
+c //= a
+print(c)
+
+c = 6
+c **= a
+print(c)
+
+c = 25
+c &= a
+print(c)
+
+c = 250
+c |= a
+print(c)
+
+c = 250
+c ^= a
+print(c)
+
+c = 2
+c >>= a
+print(c)
+
+c = 2
+c <<= a
+print(c)
+
+print(c := a)
