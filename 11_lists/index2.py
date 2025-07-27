2556c92c929bdc1b97324e2f1cf969178cee6a86
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 21:47:08 +0600

    Python List Access Negative Indexing

--- 11_lists/index2.py
+++ 11_lists/index2.py
@@ -6,3 +6,8 @@ print("\nAccess List Items")
 mylist = ["html", "css", "php", "laravel", "python", "flask", "django"]
 
 print(mylist[4])
+
+
+#Negative Indexing
+print("\nNegative Indexing")
+print(mylist[-2]) # negative indexing means right to left access
