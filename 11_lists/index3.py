02504ed1698a9da1db33af07611950bc0ae28cc0
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 00:37:11 +0600

    Python List Change a Range of Item Values

--- 11_lists/index3.py
+++ 11_lists/index3.py
@@ -6,3 +6,9 @@ print("\nChange Item Value")
 thislist = ["apple", "banana", "cherry"]
 thislist[1] = "blackcurrant"
 print(thislist)
+
+
+#Change a Range of Item Values
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
