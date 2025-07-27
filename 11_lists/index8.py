638bf990a2921e4477d297cf93f786931f1a33cf
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 04:42:11 +0600

    Python - Sort Lists Sort List Alphanumerically

---
+++ 11_lists/index8.py
@@ -0,0 +1,7 @@
+#Python - Sort Lists 
+
+#Sort List Alphanumerically
+print("\nSort List Alphanumerically")
+myalist = ["apple", "banana", "cherry", "1apple", "2banana", "3cherry"]
+myalist.sort()
+print(myalist)
