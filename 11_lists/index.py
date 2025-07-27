30397d34819553524c0519cea325c92532e8bc33
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 21:30:06 +0600

    Python List Items - Data Types

--- 11_lists/index.py
+++ 11_lists/index.py
@@ -11,3 +11,17 @@ print(tasklist[2]) #specific item
 #List Length
 print("\nList Length")
 print(len(tasklist))
+
+
+#List Items - Data Types
+print("\nList Items - Data Types")
+stringlist = ["Html", "Css", "Java", "Python"]
+intlist = [25, 36, 85, 55]
+boollist = [True, False, False, True, False, True]
+print(stringlist)
+print(intlist)
+print(boollist)
+
+print(type(stringlist))
+print(type(intlist))
+print(type(boollist))
