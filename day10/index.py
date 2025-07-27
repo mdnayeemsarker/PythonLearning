c8df23ec87c3bcd62d5951a6d0119dcd1362f226
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 14:20:52 +0600

    Python Arithmetic Operators

---
+++ day10/index.py
@@ -0,0 +1,14 @@
+#Python Operators
+
+#Arithmetic operators
+print("Arithmetic operators")
+
+print(5+6) #Addition
+print(15-6) #Subtraction
+print(15*6) #Multiplication
+print(15/6) #Division
+print(15%6) #Modulus
+print(2 ** 6) #Exponentiation
+print(25 // 2) #Floor division = the floor division // rounds the result down to the nearest whole number
+
+
