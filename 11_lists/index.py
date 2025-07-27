9435e52627185b358f0c825921338a0ffb8b7961
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-27 21:24:21 +0600

    Python Lists with list

---
+++ 11_lists/index.py
@@ -0,0 +1,8 @@
+#Python Lists
+
+#List = Lists are used to store multiple items in a single variable.
+print("\nList")
+
+tasklist = ["php", "python", "css", "html"]
+print(tasklist) #full list
+print(tasklist[2]) #specific item
