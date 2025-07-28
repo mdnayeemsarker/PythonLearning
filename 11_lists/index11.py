b129201f3974dfd463b04ce73791675b4213e255
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 15:30:06 +0600

    Python - List Methods append

---
+++ 11_lists/index11.py
@@ -0,0 +1,9 @@
+#Python - List Methods
+
+#List Methods
+oldlist = ["html", "css", "bootstrap", "php", "python"]
+mylist = ["Laravel", "flask", "Django"]
+
+print("\nappend")
+mylist.append(oldlist[2])
+print(mylist)
