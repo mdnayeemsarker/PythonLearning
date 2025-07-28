d3f854eec123c4fc42b2c84e5c5db46ba235214a
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-29 00:01:35 +0600

    Python Tuples - Create Tuple With One Item

--- 12_tuples/index.py
+++ 12_tuples/index.py
@@ -16,3 +16,11 @@ print(mytuple)
 #Tuple Length
 print("\nTuple Length")
 print(len(mytuple))
+
+
+#Create Tuple With One Item
+print("\nCreate Tuple With One Item")
+mytupleo = ("apple",)
+mytupleo1 = ("apple")
+print(type(mytupleo))
+print(type(mytupleo1))
