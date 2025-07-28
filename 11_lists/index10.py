784143468042bcacff2010ff8f186b37f7705794
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 15:27:13 +0600

    Python - Join Lists join list with extend method

--- 11_lists/index10.py
+++ 11_lists/index10.py
@@ -11,6 +11,16 @@ print(oldlist)
 
 #Another way to join two lists is by appending all the items from list2 into list1, one by one:
 print("\nanother way to join")
+oldlist = ["html", "css", "bootstrap", "php", "python"]
+mylist = ["Laravel", "flask", "Django"]
 for x in oldlist:
     mylist.append(x)
+print(mylist)
+
+
+#join list with extend method
+print("\njoin list with extend method")
+oldlist = ["html", "css", "bootstrap", "php", "python"]
+mylist = ["Laravel", "flask", "Django"]
+mylist.extend(oldlist)
 print(mylist)
