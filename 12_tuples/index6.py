c97e047943f5dd81ce4e1c6ac6e4a1d29ea6641a
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-29 05:27:31 +0600

    Python - Tuple Methods count

---
+++ 12_tuples/index6.py
@@ -0,0 +1,10 @@
+#Python - Tuple Methods
+
+#count = Returns the number of times a specified value occurs in a tuple
+print("\ncount")
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+x = tuple3.count(2)
+print(x)
+
