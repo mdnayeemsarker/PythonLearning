7ccf67205b98325da5e9eb941b4d4edaf1ad25c6
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-29 05:11:29 +0600

    Python - Loop Tuples Loop Through the Index Numbers

--- 12_tuples/index4.py
+++ 12_tuples/index4.py
@@ -5,3 +5,9 @@ print("\nLoop Through a Tuple")
 mytuple = ("apple", "banana", "cherry")
 for x in mytuple:
   print(x)
+
+
+#Loop Through the Index Numbers
+print("\nLoop Through the Index Numbers")
+for i in range(len(mytuple)):
+  print(mytuple[i])
