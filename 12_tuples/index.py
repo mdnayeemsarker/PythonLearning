c3a700d20982851953a71997baf2c89cf838c772
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 23:58:38 +0600

    Python Tuples - Allow Duplicates

--- 12_tuples/index.py
+++ 12_tuples/index.py
@@ -5,3 +5,9 @@ print("\nTuple")
 mytuple = ("apple", "banana", "cherry")
 print(mytuple)
 
+
+#Allow Duplicates
+print("\nAllow Duplicates")
+
+mytuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(mytuple)
