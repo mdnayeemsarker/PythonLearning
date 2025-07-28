7aaaca1047aea37be501fae3044da4277b4e153f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 15:33:55 +0600

    Python - List Methods count

--- 11_lists/index11.py
+++ 11_lists/index11.py
@@ -3,12 +3,11 @@
 #List Methods
 oldlist = ["html", "css", "bootstrap", "php", "python"]
 mylist = ["Laravel", "flask", "Django"]
-mylist1 = ["Laravel", "flask", "Django"]
+mylist1 = ["Laravel", "flask", "Django", "flask", "flask"]
 
 print("\nappend")
 mylist.append(oldlist[2])
 print(mylist)
-
 
 print("\nclear")
 mylist.clear()
@@ -17,3 +16,6 @@ print(mylist)
 print("\ncopy")
 mylist = mylist1.copy()
 print(mylist)
+
+print("\ncount")
+print(mylist.count("flask"))
