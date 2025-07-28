aad566e56ea543592785534b9e329d6249b730a8
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 15:32:18 +0600

    Python - List Methods copy

--- 11_lists/index11.py
+++ 11_lists/index11.py
@@ -3,6 +3,7 @@
 #List Methods
 oldlist = ["html", "css", "bootstrap", "php", "python"]
 mylist = ["Laravel", "flask", "Django"]
+mylist1 = ["Laravel", "flask", "Django"]
 
 print("\nappend")
 mylist.append(oldlist[2])
@@ -11,4 +12,8 @@ print(mylist)
 
 print("\nclear")
 mylist.clear()
+print(mylist)
+
+print("\ncopy")
+mylist = mylist1.copy()
 print(mylist)
