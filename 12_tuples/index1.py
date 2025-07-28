0d758a55110847764b7889e9a74a4f3461c79225
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-29 01:38:03 +0600

    Python Tuples - Negative Indexing

--- 12_tuples/index1.py
+++ 12_tuples/index1.py
@@ -4,3 +4,9 @@
 print("\nAccess Tuple Items")
 mytuple = ("apple", "banana", "cherry")
 print(mytuple[2])
+
+
+#Negative Indexing
+print("\nNegative Indexing")
+mytuple = ("apple", "banana", "cherry")
+print(mytuple[-1])
