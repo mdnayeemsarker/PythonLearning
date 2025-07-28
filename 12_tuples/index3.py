1f691ab834d53be43ec8cb3f22a89b28654720d1
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-29 05:03:56 +0600

    Python - Unpack Tuples Another way Unpacking a Tuple

--- 12_tuples/index3.py
+++ 12_tuples/index3.py
@@ -1,6 +1,12 @@
-#Python - Unpack Tuples Unpacking a Tuple
+#Python - Unpack Tuples 
 
 #Unpacking a Tuple
 print("\nUnpacking a Tuple")
 mytuple = ("apple", "banana", "cherry")
 print(mytuple)
+
+(green, yellow, red) = mytuple
+
+print(green)
+print(yellow)
+print(red)
