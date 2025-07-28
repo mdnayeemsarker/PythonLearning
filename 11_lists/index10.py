4cd53598c4d906a2a5515a6bd3711cf20e5116e3
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-28 15:21:03 +0600

    Python - Join Lists Join Two Lists

---
+++ 11_lists/index10.py
@@ -0,0 +1,9 @@
+#Python - Join Lists Join Two Lists
+
+#Join Two Lists
+print("\nJoin Two Lists")
+oldlist = ["html", "css", "bootstrap", "php", "python"]
+mylist = ["Laravel", "flask", "Django"]
+print(oldlist, mylist)
+oldlist = oldlist + mylist
+print(oldlist)
