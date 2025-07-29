93a96cffd6c613cc8d9c47e309d0df14ff05b989
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 03:49:47 +0600

    Python - Access Set Access check if not exist

--- 13_sets/index2.py
+++ 13_sets/index2.py
@@ -10,3 +10,7 @@ for x in myset:
 # Check if "banana" is present in the set:
 print("\ncheck if exist")
 print("banana" in myset)
+
+# Check if "banana" is NOT present in the set:
+print("\ncheck if not exist")
+print("banana" not in myset)
