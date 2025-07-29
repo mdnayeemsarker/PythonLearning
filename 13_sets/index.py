6a29ef547464283fd606fc35d1f885f707cce225
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 02:31:52 +0600

    Python Sets Duplicates Not Allowed

--- 13_sets/index.py
+++ 13_sets/index.py
@@ -6,3 +6,8 @@ print("\nSet")
 myset = {"apple", "banana", "cherry"}
 print(myset)
 
+
+#Duplicates Not Allowed
+print("\nDuplicates Not Allowed")
+myset = {"apple", "banana", "cherry", "apple"}
+print(myset)
