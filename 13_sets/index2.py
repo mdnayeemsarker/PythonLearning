9ec83427972af232d7a2bc52ab25041827196e66
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 03:48:43 +0600

    Python - Access Set Access check if exist

---
+++ 13_sets/index2.py
@@ -0,0 +1,12 @@
+#Python - Access Set Items
+
+#Access Items
+print("\nAccess Items")
+
+myset = {"apple", "banana", "cherry"}
+for x in myset:
+    print(x)
+
+# Check if "banana" is present in the set:
+print("\ncheck if exist")
+print("banana" in myset)
