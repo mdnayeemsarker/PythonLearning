21bb1bbda8371e1d722e66c81a61e50fd4a6bfa2
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 04:17:23 +0600

    Python - Remove Set Items Remove Item

---
+++ 13_sets/index4.py
@@ -0,0 +1,12 @@
+#Python - Remove Set Items 
+
+#Remove Item
+print("\nRemove Item")
+
+myset = {"apple", "banana", "cherry"}
+myset.add("orange")
+print(myset)
+myset.remove("banana") #it remove easily if the item is exist
+print(myset)
+# myset.remove("remove") #it will gettign issue
+print(myset)
