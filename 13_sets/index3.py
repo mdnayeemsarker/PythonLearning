fe03823e3a1a8c20fb4ce5a367199285b2627e01
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 03:59:37 +0600

    Python - Add Set Items Add Items Add Set

--- 13_sets/index3.py
+++ 13_sets/index3.py
@@ -6,3 +6,10 @@ print("\nAdd Items")
 myset = {"apple", "banana", "cherry"}
 myset.add("orange")
 print(myset)
+
+
+#Add Set
+print("\nAdd Set")
+tropical = {"pineapple", "mango", "papaya"}
+myset.update(tropical)
+print(myset)
