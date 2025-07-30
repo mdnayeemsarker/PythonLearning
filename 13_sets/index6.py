61a822abb4c18cf31f4482f2ef095fe4f3149e71
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 16:50:15 +0600

    Python - Join Sets Keep the items that exist in both set1, and set2

--- 13_sets/index6.py
+++ 13_sets/index6.py
@@ -60,3 +60,12 @@ print(set3)
 print("\nUse & to join two sets:")
 set3 = set1 & set2
 print(set3)
+
+#Keep the items that exist in both set1, and set2
+print("\nKeep the items that exist in both set1, and set2")
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.intersection_update(set2)
+
+print(set1)
