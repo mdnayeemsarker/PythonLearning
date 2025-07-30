f8e9967c7b9f70bc4c3fd9e386d4b19b9171e44e
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-31 00:04:03 +0600

    Python - Dictionary Items get keyes only

--- 14_dictionaries/index2.py
+++ 14_dictionaries/index2.py
@@ -12,3 +12,7 @@ print(mydict["model"])
 #There is also a method called get() that will give you the same result:
 print("\nanother way to use get method")
 print(mydict.get("model"))
+
+#get keyes only
+print("\nget keyes")
+print(mydict.keys())
