19aa636f593307271d3d2827624874636bb2a162
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 16:25:04 +0600

    Python - Join Sets Join Multiple Sets

--- 13_sets/index6.py
+++ 13_sets/index6.py
@@ -11,3 +11,14 @@ print(set2)
 #You can use the | operator instead of the union() method, and you will get the same result.
 set3 = myset | set1
 print(set3)
+
+
+#Join Multiple Sets
+print("\nJoin Multiple Sets")
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+myset = set1.union(set2, set3, set4)
+print(myset)
