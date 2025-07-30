4cf9edcf7ffea3bfc95defffd8d091a4bd791e78
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 16:48:36 +0600

    Python - Join Sets Intersection

--- 13_sets/index6.py
+++ 13_sets/index6.py
@@ -46,3 +46,12 @@ print(z)
 print("\nUpdate")
 set1.update(set2)
 print(set1)
+
+
+#Intersection = The intersection() method will return a new set, that only contains the items that are present in both sets.
+print("\nIntersection")
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.intersection(set2)
+print(set3)
