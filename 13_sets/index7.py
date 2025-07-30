84d4ad2784c750eafdb9243f31218c4ab3fc09e9
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 17:03:51 +0600

    Python - Set Methods clear method

--- 13_sets/index7.py
+++ 13_sets/index7.py
@@ -6,3 +6,10 @@ print("\nadd method")
 myset = {"apple", "banana", "cherry"}
 myset.add("orange")
 print(myset)
+
+
+#clear = Removes all the elements from the set
+print("\nclear")
+fruits = {"apple", "banana", "cherry"}
+fruits.clear()
+print(fruits)
