58d15c49ac7274d9f0659393fb40d90c01d3743b
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 17:11:14 +0600

    Python - Set Methods intersection_update method

--- 13_sets/index7.py
+++ 13_sets/index7.py
@@ -51,3 +51,11 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 z = x.intersection(y)
 print(z)
+
+
+#intersection_update = Removes the items in this set that are not present in other, specified set(s)
+print("\nintersection_update")
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)
