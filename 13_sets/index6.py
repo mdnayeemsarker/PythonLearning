463943661f6fc4847a2dcfdddccf44ad31100cc8
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-30 16:23:51 +0600

    Python - Join Sets use operator |

--- 13_sets/index6.py
+++ 13_sets/index6.py
@@ -8,3 +8,6 @@ set1 = {1, 2, 3}
 set2 = myset.union(set1)
 print(set2)
 
+#You can use the | operator instead of the union() method, and you will get the same result.
+set3 = myset | set1
+print(set3)
