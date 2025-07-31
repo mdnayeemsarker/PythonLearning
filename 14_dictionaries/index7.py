b6d02a1bf14e4384086b686f01ad1e8011d1f6b4
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 02:30:01 +0600

    Python - Copy Dictionaries Copy a Dictionary

---
+++ 14_dictionaries/index7.py
@@ -0,0 +1,11 @@
+#Python - Copy Dictionaries 
+
+#Copy a Dictionary
+print("\nCopy a Dictionary")
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict1 = mydict.copy()
+print(mydict1)
