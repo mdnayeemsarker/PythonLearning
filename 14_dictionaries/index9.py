be34740e170bf54d5e65222377ed0e96993e8aff
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 02:50:20 +0600

    Python - Dictionary Methods clear

---
+++ 14_dictionaries/index9.py
@@ -0,0 +1,11 @@
+#Python - Dictionary Methods
+
+#clear = 	Removes all the elements from the dictionary
+print("\nclear")
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+print(car)
