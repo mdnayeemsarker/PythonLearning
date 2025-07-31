f682a0fa0ac1c42521b14dd8c33bde03288fa267
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 02:31:51 +0600

    Python - Copy Dictionaries Another way to copy a Dictionary

--- 14_dictionaries/index7.py
+++ 14_dictionaries/index7.py
@@ -9,3 +9,14 @@ mydict = {
 }
 mydict1 = mydict.copy()
 print(mydict1)
+
+
+#Another way to make a copy is to use the built-in function dict().
+print("\nAnother way to copy a Dictionary")
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict1 = dict(mydict)
+print(mydict1)
