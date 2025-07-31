dabf7af9998765724c34b5f838bfe7e6f45cef17
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 01:32:27 +0600

    Python - Remove Dictionary Items Another option Removing items with popitem

--- 14_dictionaries/index5.py
+++ 14_dictionaries/index5.py
@@ -10,3 +10,8 @@ mydict = {
 print(mydict)
 mydict.pop("year")
 print(mydict)
+
+#popitem = The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+print("\npopitem")
+mydict.popitem()
+print(mydict)
