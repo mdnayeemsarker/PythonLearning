eb8b5bc500427d0e8f4c684ed8af79fbd84fdf39
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 01:25:06 +0600

    Python - Add Dictionary Items Adding Items

---
+++ 14_dictionaries/index4.py
@@ -0,0 +1,12 @@
+#Python - Add Dictionary Items 
+
+#Adding Items
+print("\nAdding Items")
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(mydict)
+mydict["color"] = "red"
+print(mydict)
