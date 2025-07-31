e1f973e42b7ab7f4f8364bc90affa64c0db638c4
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-31 08:19:06 +0600

    Python - Change Dictionary Items Change Values

---
+++ 14_dictionaries/index3.py
@@ -0,0 +1,12 @@
+#Python - Change Dictionary Items
+
+#Change Values
+print("\nChange Values")
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(mydict)
+mydict["year"] = 2018
+print(mydict)
