3b17229beafa092fa341d4467966b0916d840444
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-31 08:14:55 +0600

    Python - Dictionary Items Get Items

--- 14_dictionaries/index2.py
+++ 14_dictionaries/index2.py
@@ -29,3 +29,16 @@ x = car.values()
 print(x) #before the change
 car["year"] = 2020
 print(x) #after the change
+
+
+#Get Items
+print("\nGet Items")
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
