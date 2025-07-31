4254b83630476ebcb11e8868ba742f317f358f86
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 02:53:04 +0600

    Python - Dictionary Methods get

--- 14_dictionaries/index9.py
+++ 14_dictionaries/index9.py
@@ -28,3 +28,14 @@ x = ('key1', 'key2', 'key3')
 y = 0
 thisdict = dict.fromkeys(x, y)
 print(thisdict)
+
+
+#get = Returns the value of the specified key
+print("\nget")
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.get("model")
+print(x)
