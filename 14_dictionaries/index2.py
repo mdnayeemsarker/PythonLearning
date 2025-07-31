f500bdb719c31f3000736118caabd779de8dfe4e
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-07-31 08:01:38 +0600

    Python - Dictionary Items Get Values

--- 14_dictionaries/index2.py
+++ 14_dictionaries/index2.py
@@ -16,3 +16,16 @@ print(mydict.get("model"))
 #get keyes only
 print("\nget keyes")
 print(mydict.keys())
+
+
+#Get Values
+print("\nGet Values")
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
