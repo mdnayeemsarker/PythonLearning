e79d34cd90a1788b726db79e3604141b07ba7311
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 01:29:46 +0600

    Python - Remove Dictionary Items Removing Items

---
+++ 14_dictionaries/index5.py
@@ -0,0 +1,12 @@
+#Python - Remove Dictionary Items Removing Items
+
+#Removing Items
+print("\nRemoving Items")
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(mydict)
+mydict.pop("year")
+print(mydict)
