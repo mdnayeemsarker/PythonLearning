43cf3f281c5a6da91e350a28c62f89070a35f84f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-01 01:59:59 +0600

    Python - lop Directories Print all key names in the dictionary, one by one:

---
+++ 14_dictionaries/index6.py
@@ -0,0 +1,12 @@
+#Python - Loop Dictionaries
+
+#Loop Through a Dictionary
+#Print all key names in the dictionary, one by one:
+print("\nPrint all key names in the dictionary")
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in mydict:
+  print(x)
