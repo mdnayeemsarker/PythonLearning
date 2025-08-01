501501e82d020e3289a9ef1e849e8cbbd27e0a2b
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-02 01:05:59 +0600

    Python Conditions Not logical operator

--- 15_if_else/index.py
+++ 15_if_else/index.py
@@ -82,3 +82,10 @@ if a > b and c > a:
 print("\nOr")
 if a > b or a > c:
   print("At least one of the conditions is True")
+
+#Not
+print("\nNot")
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
