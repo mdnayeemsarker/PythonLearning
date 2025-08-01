4a695d8daf6b91d601d0985e41db75b0d11d3414
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-02 01:06:57 +0600

    Python Conditions Nested If

--- 15_if_else/index.py
+++ 15_if_else/index.py
@@ -89,3 +89,15 @@ a = 33
 b = 200
 if not a > b:
   print("a is NOT greater than b")
+
+
+#Nested If
+print("\nNested If")
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
