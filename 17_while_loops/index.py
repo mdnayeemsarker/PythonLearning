ac353f78fcba1e8fb1de22e42452abf72e116908
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-02 04:14:22 +0600

    Python While Loops - The while Loop

---
+++ 17_while_loops/index.py
@@ -0,0 +1,12 @@
+#Python While Loops
+
+#Python has two primitive loop commands:
+  # while loops
+  # for loops
+
+#The while Loop
+print("\nThe while Loop")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
