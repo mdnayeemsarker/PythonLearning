c7c12d09efa5934aa5f9b16eec94a71eb7b1c54c
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-02 04:20:25 +0600

    Python For Loops - For loop

---
+++ 18_for_loops/index.py
@@ -0,0 +1,11 @@
+#Python For Loops
+
+#Python has two primitive loop commands:
+  # while loops
+  # for loops
+
+#Python For Loops
+print("\nPython For Loops")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
