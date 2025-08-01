f0dcdb966db13377471d931f84e83a52c4be2a0f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-02 05:16:19 +0600

    Python Functions - Parameters or Arguments

--- 19_functions/index.py
+++ 19_functions/index.py
@@ -19,3 +19,12 @@ def my_function(fname):
 my_function("Emil")
 my_function("Tobias")
 my_function("Linus")
+
+
+#Parameters or Arguments?
+# The terms parameter and argument can be used for the same thing: information that are passed into a function.
+# From a function's perspective:
+# A parameter is the variable listed inside the parentheses in the function definition.
+# An argument is the value that is sent to the function when it is called.
+
+
