432367285a202082baa217e27a6e41a62cfbe935
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-02 00:53:03 +0600

    Python Conditions and If statements

---
+++ 15_if_else/index.py
@@ -0,0 +1,15 @@
+#Python If ... Else
+
+#Python Conditions and If statements
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+print("\nIf statement:")
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
