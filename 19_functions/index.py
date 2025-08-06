a2a0fc1d1e8d3883997f6a19f2633d355bf41cd4
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-06 17:28:29 +0600

    Python Functions - Keyword Arguments

--- 19_functions/index.py
+++ 19_functions/index.py
@@ -40,3 +40,11 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+
+#Keyword Arguments
+print ("\nKeyword Arguments")
+def my_function1(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function1(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
