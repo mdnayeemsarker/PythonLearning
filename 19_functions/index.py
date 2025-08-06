ff0cb3c20fe552a8f62f6dc2a1e97d0d2955aa17
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-07 01:14:36 +0600

    Python Functions - Recursion

--- 19_functions/index.py
+++ 19_functions/index.py
@@ -122,3 +122,17 @@ def my_function(a, b, /, *, c, d):
   print(a + b + c + d)
   
 my_function(5, 6, c = 7, d = 8)
+
+
+#Recursion
+print('\nRecursion')
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
