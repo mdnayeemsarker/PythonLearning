42c0f58156fee6df9750b37cc975ab5d8cd4a1a9
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-06 23:38:28 +0600

    Python Functions - Arbitrary Keyword Arguments

--- 19_functions/index.py
+++ 19_functions/index.py
@@ -48,3 +48,10 @@ def my_function1(child3, child2, child1):
   print("The youngest child is " + child3)
 
 my_function1(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+#Arbitrary Keyword Arguments, **kwargs
+print('\nArbitrary Keyword Arguments, **kwargs')
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+my_function(fname = "Tobias", lname = "Refsnes")
