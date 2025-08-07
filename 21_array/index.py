ea5ca9e2cdae7757c2822dfee4d28bf906bf899d
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-07 21:18:11 +0600

    Python Arrays - The Length of an Array

--- 21_array/index.py
+++ 21_array/index.py
@@ -9,3 +9,8 @@ print(cars)
 #Access the Elements of an Array
 print('\nAccess the Elements of an Array')
 print(cars[1])
+
+
+#The Length of an Array
+print('\nThe Length of an Array')
+print(len(cars))
