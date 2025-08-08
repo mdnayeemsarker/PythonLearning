9cb301c8a292ba8f0e7fa559b19927655c32413a
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 00:51:23 +0600

    Python Class/Objects - init method

--- 23_class_objects/index.py
+++ 23_class_objects/index.py
@@ -12,3 +12,16 @@ print(MyClass)
 print('\nObject')
 p1 = MyClass()
 print(p1.x)
+
+
+#The __init__() Method
+print('\nThe __init__() Method')
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
