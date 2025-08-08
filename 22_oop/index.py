fdc35ac3cab6d5b9354affab931a08fb9031cb44
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 00:43:55 +0600

    Python OOP

---
+++ 22_oop/index.py
@@ -0,0 +1,15 @@
+#Python OOP
+
+#oop
+print('\noop')
+# Advantages of OOP
+  # Provides a clear structure to programs
+  # Makes code easier to maintain, reuse, and debug
+  # Helps keep your code DRY (Don't Repeat Yourself)
+  # Allows you to build reusable applications with less code
+
+
+# Class	  Objects
+# ------------------------------
+# Fruit	  Apple, Banana, Mango
+# Car	    Volvo, Audi, Toyota
