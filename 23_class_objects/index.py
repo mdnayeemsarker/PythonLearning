cf1c07fe4e36cd110fce4570f51d72f7cbca1d1d
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 00:54:31 +0600

    Python Class/Objects - The self Parameter

--- 23_class_objects/index.py
+++ 23_class_objects/index.py
@@ -63,3 +63,17 @@ class Person:
 
 p1 = Person("John", 36)
 p1.myfunc()
+
+
+#The self Parameter
+print('\nThe self Parameter')
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
