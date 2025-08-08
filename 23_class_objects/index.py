3a0a926ea6529c614501b02438636ea8d5170603
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 00:53:51 +0600

    Python Class/Objects - create method

--- 23_class_objects/index.py
+++ 23_class_objects/index.py
@@ -49,3 +49,17 @@ class Person:
 
 p1 = Person("John", 36)
 print(p1)
+
+
+#Create Methods
+print('\nCreate Methods')
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
