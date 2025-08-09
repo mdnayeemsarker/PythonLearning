2e06ad96911d6886ca9113f7e5793242478a54e7
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 16:32:16 +0600

    Python Inheritance - use super function

--- 24_inheritance/index.py
+++ 24_inheritance/index.py
@@ -41,3 +41,22 @@ class Student(Person):
 
 x = Student("Mike", "Olsen")
 x.printname()
+
+
+
+#Use the super() Function
+print('\nUse the super() Function')
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+  
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+x = Student("Nayeem", "Sarker")
+x.printname()
