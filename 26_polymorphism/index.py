e655ede387c613078e8a9e6d612744a73f8939e6
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 18:18:22 +0600

    Python Polymorphism - Function Polymorphism - String

---
+++ 26_polymorphism/index.py
@@ -0,0 +1,9 @@
+#Python Polymorphism
+
+#Function Polymorphism
+print('\nFunction Polymorphism')
+# An example of a Python function that can be used on different objects is the len() function.
+print('\nstring')
+#String = For strings len() returns the number of characters:
+x = "Hello World!"
+print(len(x))
