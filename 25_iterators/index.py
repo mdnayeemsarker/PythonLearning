a769e939c764677df0125dfdf64dd257e8d5b5e6
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 17:55:00 +0600

    Python Iterators - another Iterator vs Iterable

--- 25_iterators/index.py
+++ 25_iterators/index.py
@@ -15,3 +15,14 @@ myit = iter(mytuple)
 print(next(myit))
 print(next(myit))
 print(next(myit))
+
+print('\nanother')
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
