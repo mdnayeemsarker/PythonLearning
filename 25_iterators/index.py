af004a30dcb895f5c8ff82d5ff3a0377a04d065c
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 17:54:04 +0600

    Python Iterators - Iterator vs Iterable

--- 25_iterators/index.py
+++ 25_iterators/index.py
@@ -6,3 +6,12 @@ print('\nPython Iterators')
 # An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
 # Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().
 
+
+#Iterator vs Iterable
+print('\nIterator vs Iterable')
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
