95c080ee090c60a6ff6b935b040b091fab4eecb3
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-08-09 18:09:16 +0600

    Python Iterators - Create an Iterator

--- 25_iterators/index.py
+++ 25_iterators/index.py
@@ -41,3 +41,25 @@ mystr = "banana"
 
 for x in mystr:
   print(x)
+
+
+#Create an Iterator
+print('\nCreate an Iterator')
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
