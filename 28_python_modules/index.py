01859bb27f025504f113cacc75ebf3f11eb79810
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-12 01:26:55 +0600

    Python Modules - Create a Module

---
+++ 28_python_modules/index.py
@@ -0,0 +1,9 @@
+#Python Modules
+
+#Consider a module to be the same as a code library.
+#A file containing a set of functions you want to include in your application.
+print('\nCreate a Module')
+import newmodule 
+def greeting(name):
+  print("Hello, " + name)
+newmodule.greeting("Jonathan")
