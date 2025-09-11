cf4212a4aab79568f1c120ca50997ab409e7899f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-12 01:38:54 +0600

    Python Modules - Naming and Renaming a Module

--- 28_python_modules/index.py
+++ 28_python_modules/index.py
@@ -11,3 +11,15 @@ newmodule.greeting("Jonathan")
 print('\nVariables in Module')
 a = newmodule.person1["age"]
 print(a)
+
+
+#Naming a Module
+# You can name the module file whatever you like, but it must have the file extension .py 
+# Re-naming a Module
+# You can create an alias when you import a module, by using the as keyword:
+print('\nNaming a Module')
+import newmodule as mx
+
+print('\nRe-naming a Module')
+a = mx.person1["name"]
+print(a)
