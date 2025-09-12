7662be404bc353f08cab44285486674a92a4d312
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-12 23:50:58 +0600

    Python Datetime - Python Dates

---
+++ 29_python_datetime/index.py
@@ -0,0 +1,7 @@
+#Python Datetime
+
+#A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as date objects.
+print('\nPython Dates')
+import datetime
+x = datetime.datetime.now()
+print(x)
