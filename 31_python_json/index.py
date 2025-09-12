0ddb746da40164f4b965c31ddd9b87978c8e839d
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-13 02:23:15 +0600

    Python JSON - Another Example Convert python objects of following types

--- 31_python_json/index.py
+++ 31_python_json/index.py
@@ -38,3 +38,20 @@ print(json.dumps(31.76))
 print(json.dumps(True))
 print(json.dumps(False))
 print(json.dumps(None))
+
+#Another Example
+print('\nAnother Example')
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
