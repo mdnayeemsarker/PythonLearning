2c6abb82c5d422d70927a73acf7db0a416701dac
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-13 02:19:21 +0600

    Python JSON - Convert from JSON to Python

--- 31_python_json/index.py
+++ 31_python_json/index.py
@@ -4,3 +4,12 @@
 #Python has a built-in package called json, which can be used to work with JSON data.
 print('\nJSON in Python')
 import json
+
+#Convert from JSON to Python
+print('\nConvert from JSON to Python')
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+# parse x:
+y = json.loads(x)
+# the result is a Python dictionary:
+print(y["age"])
