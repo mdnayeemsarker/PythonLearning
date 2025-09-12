9e3d331f91ad8ed54ec2eb2edb0c4c879f81cf93
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-13 02:26:03 +0600

    Python JSON - Format the Result use seperate boject with seperator

--- 31_python_json/index.py
+++ 31_python_json/index.py
@@ -73,3 +73,22 @@ x = {
 
 # use four indents to make it easier to read the result:
 print(json.dumps(x, indent=4))
+
+
+#Use the separators parameter to change the default separator:
+print('\nUse the seperate parameter to convert the default separetor')
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
