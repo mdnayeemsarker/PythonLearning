bc5fb4ef4db1a2bc3e7582f0ef5f9c7be3b13ffc
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-27 19:22:03 +0600

    Python Try Except - Finally

--- 34_try_except/index.py
+++ 34_try_except/index.py
@@ -38,3 +38,13 @@ except:
   print("Something went wrong")
 else:
   print("Nothing went wrong")
+
+
+#Finally
+print("\nFinally")
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
