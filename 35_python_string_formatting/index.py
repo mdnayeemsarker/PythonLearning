8a6385cd5a38bcd11e7d7227e7efa4c1e2024474
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-27 19:34:58 +0600

    Python String Formatting - Placeholders and Modifiers

--- 35_python_string_formatting/index.py
+++ 35_python_string_formatting/index.py
@@ -4,3 +4,10 @@
 print("F-Strings")
 txt = f"The price is 49 dollars"
 print(txt)
+
+
+#Placeholders and Modifiers
+print("\nPlaceholders and Modifiers")
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
