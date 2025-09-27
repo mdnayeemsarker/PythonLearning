705e50818f52707d6f1d7072e9ad54903e14b5e6
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-27 19:24:13 +0600

    Python Try Except - Raise an exception

--- 34_try_except/index.py
+++ 34_try_except/index.py
@@ -62,3 +62,10 @@ try:
     f.close()
 except:
   print("Something went wrong when opening the file")
+
+
+#Raise an exception
+print("\nRaise an exception")
+x = -1
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
