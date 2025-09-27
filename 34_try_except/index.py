2b98db35f731a5448e80acb6fdd15c2c606e9fc5
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-27 18:48:24 +0600

    Python Try Except - Exception Handling

---
+++ 34_try_except/index.py
@@ -0,0 +1,13 @@
+#Python Try Except
+
+# The try block lets you test a block of code for errors.
+# The except block lets you handle the error.
+# The else block lets you execute code when there is no error.
+# The finally block lets you execute code, regardless of the result of the try- and except blocks.
+
+#Exception Handling
+print("Exception Handling\n")
+try:
+  print(x)
+except:
+  print("An exception occurred")
