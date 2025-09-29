447789544061c9f3a9a06d671cb35c6d952b21ae
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 01:27:41 +0600

    Python User input - using prompt

--- 37_user_input/index.py
+++ 37_user_input/index.py
@@ -4,3 +4,8 @@ print("User Input")
 print("Enter your name:")
 name = input()
 print(f"Hello {name}")
+
+#Using prompt
+print("\nUsing prompt")
+name = input("Enter your name: ")
+print(f"Hello {name}")
