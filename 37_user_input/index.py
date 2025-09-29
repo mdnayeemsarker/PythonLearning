0f616c5c5ad9a1b76d6178c20fb71fcda938d01d
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 01:35:00 +0600

    Python User input - Input Number

--- 37_user_input/index.py
+++ 37_user_input/index.py
@@ -18,3 +18,12 @@ fav1 = input("What is your favorite animal:")
 fav2 = input("What is your favorite color:")
 fav3 = input("What is your favorite number:")
 print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+
+
+#Input Number
+print("\nInput Number")
+import math
+x = input("Enter a number:")
+#find the square root of the number:
+y = math.sqrt(float(x))
+print(f"The square root of {x} is {y}")
