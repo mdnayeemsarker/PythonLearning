8612cf59f39976450a7a7e23bf25fafcd9297249
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 01:19:56 +0600

    Python None - Comparing to none

--- 36_none/index.py
+++ 36_none/index.py
@@ -7,3 +7,11 @@ print(x)
 print("\nNone Type")
 x = None
 print(type(x))
+
+#Comparing to None
+print("\nComparing to None")
+x = None
+if x is None:
+  print("Yes, x is None")
+else:
+  print("No, x is not None")
