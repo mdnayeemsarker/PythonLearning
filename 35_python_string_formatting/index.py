da6f588613cf983bed513c97773cb85d32a96900
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 01:10:35 +0600

    Python String Formatting - Index Numbers another example

--- 35_python_string_formatting/index.py
+++ 35_python_string_formatting/index.py
@@ -94,3 +94,11 @@ itemno = 567
 price = 49
 myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
+
+
+#Index Numbers another example
+print("\nIndex Numbers another example")
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
