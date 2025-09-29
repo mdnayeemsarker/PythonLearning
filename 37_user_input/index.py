23087e9b87b9cff27b4df6e2cf127fa0a86c510c
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 01:39:41 +0600

    Python User input - Validate Input

--- 37_user_input/index.py
+++ 37_user_input/index.py
@@ -27,3 +27,17 @@ x = input("Enter a number:")
 #find the square root of the number:
 y = math.sqrt(float(x))
 print(f"The square root of {x} is {y}")
+
+
+#Validate Input
+print("\nValidate Input")
+y = True
+while y == True:
+  x = input("Enter a number:")
+  try:
+    x = float(x);
+    y = False
+  except:
+    print("Wrong input, please try again.")
+
+print("Thank you!")
