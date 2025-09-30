7e45684cb07c0c8a69c42aa6b1f3a551f93d8576
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 04:38:53 +0600

    Python DSA - Bubble Sort Improved Bubble Sort algorithm

--- 43_dsa/14bubble_sort.py
+++ 43_dsa/14bubble_sort.py
@@ -21,3 +21,19 @@ for i in range(n-1):
       mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
 
 print(mylist)
+
+#Improved Bubble Sort algorithm
+print("\nImproved Bubble Sort algorithm")
+mylist = [7, 3, 9, 12, 11]
+
+n = len(mylist)
+for i in range(n-1):
+  swapped = False
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+      swapped = True
+  if not swapped:
+    break
+
+print(mylist)
