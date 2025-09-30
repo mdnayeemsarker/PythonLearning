a06a40f8245d0b835516e10b624f0fc3bae863a7
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 02:58:24 +0600

    Python DSA - Creating a stack using class

--- 43_dsa/03stacks.py
+++ 43_dsa/03stacks.py
@@ -35,3 +35,42 @@ print("isEmpty: ", isEmpty)
 
 # Size
 print("Size: ",len(stack))
+
+#Creating a stack using class
+print("\nCreating a stack using class")
+class Stack:
+  def __init__(self):
+    self.stack = []
+
+  def push(self, element):
+    self.stack.append(element)
+
+  def pop(self):
+    if self.isEmpty():
+      return "Stack is empty"
+    return self.stack.pop()
+
+  def peek(self):
+    if self.isEmpty():
+      return "Stack is empty"
+    return self.stack[-1]
+
+  def isEmpty(self):
+    return len(self.stack) == 0
+
+  def size(self):
+    return len(self.stack)
+
+# Create a stack
+myStack = Stack()
+
+myStack.push('A')
+myStack.push('B')
+myStack.push('C')
+
+print("Stack: ", myStack.stack)
+print("Pop: ", myStack.pop())
+print("Stack after Pop: ", myStack.stack)
+print("Peek: ", myStack.peek())
+print("isEmpty: ", myStack.isEmpty())
+print("Size: ", myStack.size())
