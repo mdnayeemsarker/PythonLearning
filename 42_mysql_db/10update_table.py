df03c883641138b696b880c0e23dc51ccb481622
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 01:48:38 +0600

    Python Update Table Prevent SQL Injection

--- 42_mysql_db/10update_table.py
+++ 42_mysql_db/10update_table.py
@@ -14,3 +14,11 @@ sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'"
 mycursor.execute(sql)
 mydb.commit()
 print(mycursor.rowcount, "record(s) affected")
+
+#Prevent SQL Injection
+print("\nPrevent SQL Injection")
+sql = "UPDATE customers SET address = %s WHERE address = %s"
+val = ("Valley 345", "Canyon 124")
+mycursor.execute(sql, val)
+mydb.commit()
+print(mycursor.rowcount, "record(s) affected")
