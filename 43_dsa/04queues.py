b899176cd429c500bc0e627954541ed85b019188
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 03:05:50 +0600

    Python DSA - Implementing a Queue Class

--- 43_dsa/04queues.py
+++ 43_dsa/04queues.py
@@ -32,3 +32,43 @@ print("isEmpty: ", isEmpty)
 
 # Size
 print("Size: ", len(queue))
+
+
+#Implementing a Queue Class
+print("\nImplementing a Queue Class")
+class Queue:
+  def __init__(self):
+    self.queue = []
+    
+  def enqueue(self, element):
+    self.queue.append(element)
+
+  def dequeue(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.queue.pop(0)
+
+  def peek(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.queue[0]
+
+  def isEmpty(self):
+    return len(self.queue) == 0
+
+  def size(self):
+    return len(self.queue)
+
+# Create a queue
+myQueue = Queue()
+
+myQueue.enqueue('A')
+myQueue.enqueue('B')
+myQueue.enqueue('C')
+
+print("Queue: ", myQueue.queue)
+print("Peek: ", myQueue.peek())
+print("Dequeue: ", myQueue.dequeue())
+print("Queue after Dequeue: ", myQueue.queue)
+print("isEmpty: ", myQueue.isEmpty())
+print("Size: ", myQueue.size())
