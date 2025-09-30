aa561a8367f05a3bccc85f96aaa0e545d887bb15
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 02:37:14 +0600

    Python DSA - dsa with python

---
+++ 43_dsa/01dsa.py
@@ -0,0 +1,31 @@
+#DSA with Python
+
+#Data Structures
+# Data Structures are a way of storing and organizing data in a computer.
+# Python has built-in support for several data structures, such as lists, dictionaries, and sets.
+# Other data structures can be implemented using Python classes and objects, such as linked lists, stacks, queues, trees, and graphs.
+# In this tutorial we will concentrate on these Data Structures:
+# Lists and Arrays
+#   Stacks
+#   Queues
+#   Linked Lists
+#   Hash Tables
+#   Trees
+#   Binary Trees
+#   Binary Search Trees
+#   AVL Trees
+# Graphs
+
+
+#Algorithms
+# Algorithms are a way of working with data in a computer and solving problems like sorting, searching, etc.
+# In this tutorial we will concentrate on these search and sort Algorithms:
+# Linear Search
+# Binary Search
+# Bubble Sort
+# Selection Sort
+# Insertion Sort
+# Quick Sort
+# Counting Sort
+# Radix Sort
+# Merge Sort
