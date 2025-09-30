1199e43095fd4c7a6aa3f7a21268fbc2a2f5a85f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 18:47:33 +0600

    Python Delete File - Delete Folder

--- 41_file_delete/index.py
+++ 41_file_delete/index.py
@@ -12,3 +12,15 @@ if os.path.exists("/home/abmn/Desktop/Python/41_file_delete/newfile.txt"):
   os.remove("/home/abmn/Desktop/Python/41_file_delete/newfile.txt")
 else:
   print("The file does not exist")
+
+#Delete Folder
+print("\nDelete Folder:")
+#To delete an entire folder, use the os.rmdir() method
+# os.rmdir("/home/abmn/Desktop/Python/41_file_delete/myfolder") 
+#Folder should be empty 
+#Check if Folder exist
+print("\nCheck if Folder exist:")
+if os.path.exists("/home/abmn/Desktop/Python/41_file_delete/myfolder"):
+  os.rmdir("/home/abmn/Desktop/Python/41_file_delete/myfolder")
+else:
+  print("The folder does not exist")
