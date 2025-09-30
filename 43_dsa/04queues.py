df9a81f695456e4bef37f741db133e8022f3d344
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 03:05:04 +0600

    Python DSA - Queue Implementation using Python Lists

---
+++ 43_dsa/04queues.py
@@ -0,0 +1,34 @@
+#Queues with Python
+
+# Enqueue: Adds a new element to the queue.
+# Dequeue: Removes and returns the first (front) element from the queue.
+# Peek: Returns the first element in the queue.
+# isEmpty: Checks if the queue is empty.
+# Size: Finds the number of elements in the queue.
+
+# Queue Implementation using Python Lists
+#Queues
+queue = []
+
+# Enqueue
+queue.append('A')
+queue.append('B')
+queue.append('C')
+print("Queue: ", queue)
+
+# Peek
+frontElement = queue[0]
+print("Peek: ", frontElement)
+
+# Dequeue
+poppedElement = queue.pop(0)
+print("Dequeue: ", poppedElement)
+
+print("Queue after Dequeue: ", queue)
+
+# isEmpty
+isEmpty = not bool(queue)
+print("isEmpty: ", isEmpty)
+
+# Size
+print("Size: ", len(queue))
