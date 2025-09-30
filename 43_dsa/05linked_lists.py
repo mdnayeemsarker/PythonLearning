4daea8b8e8977ff4dc9eccf1b844d1e5ef94ea86
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 03:20:29 +0600

    Python DSA - Link List Find The Lowest Value in a Linked List

--- 43_dsa/05linked_lists.py
+++ 43_dsa/05linked_lists.py
@@ -14,6 +14,7 @@
   #Sort
 
 #Traversal of a Linked List
+print("Traversal of a Linked List")
 class Node:
   def __init__(self, data):
     self.data = data
@@ -38,3 +39,32 @@ node3.next = node4
 node4.next = node5
 
 traverseAndPrint(node1)
+
+#Find The Lowest Value in a Linked List
+print("\nFind The Lowest Value in a Linked List")
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def findLowestValue(head):
+  minValue = head.data
+  currentNode = head.next
+  while currentNode:
+    if currentNode.data < minValue:
+      minValue = currentNode.data
+    currentNode = currentNode.next
+  return minValue
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+print("The lowest value in the linked list is:", findLowestValue(node1))
