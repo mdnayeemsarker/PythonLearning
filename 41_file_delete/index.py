933980e2c5d3e08dafb1609752cb01e34149c9e8
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 18:46:39 +0600

    Python Delete File - Check if File exist

--- 41_file_delete/index.py
+++ 41_file_delete/index.py
@@ -4,4 +4,11 @@
 # To delete a file, you must import the OS module, and run its os.remove() function
 print("Delete a File:")
 import os
-os.remove("/home/abmn/Desktop/Python/41_file_delete/newfile.txt")
+# os.remove("/home/abmn/Desktop/Python/41_file_delete/newfile.txt")
+
+#Check if File exist
+print("\nCheck if File exist:")
+if os.path.exists("/home/abmn/Desktop/Python/41_file_delete/newfile.txt"):
+  os.remove("/home/abmn/Desktop/Python/41_file_delete/newfile.txt")
+else:
+  print("The file does not exist")
