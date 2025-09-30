ed35e7f64b05c2a9e21d99a8c361dc3dbcda006f
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 03:54:10 +0600

    Python DSA - Binary Search Tree Search for a Value in a BST

--- 43_dsa/09binary_search_trees.py
+++ 43_dsa/09binary_search_trees.py
@@ -38,3 +38,48 @@ node19.left = node18
 
 # Traverse
 inOrderTraversal(root)
+
+#Search for a Value in a BST
+print("\n\nSearch for a Value in a BST")
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+def search(node, target):
+  if node is None:
+    return None
+  elif node.data == target:
+    return node
+  elif target < node.data:
+    return search(node.left, target)
+  else:
+    return search(node.right, target)
+
+root = TreeNode(13)
+node7 = TreeNode(7)
+node15 = TreeNode(15)
+node3 = TreeNode(3)
+node8 = TreeNode(8)
+node14 = TreeNode(14)
+node19 = TreeNode(19)
+node18 = TreeNode(18)
+
+root.left = node7
+root.right = node15
+
+node7.left = node3
+node7.right = node8
+
+node15.left = node14
+node15.right = node19
+
+node19.left = node18
+
+# Search for a value
+result = search(root, 13)
+if result:
+  print(f"Found the node with value: {result.data}")
+else:
+  print("Value not found in the BST.")
