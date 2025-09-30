6ee80870b797496dd375a7792be366c15e83b83a
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 02:42:06 +0600

    Python DSA - creating list

---
+++ 43_dsa/02lists_arrays.py
@@ -0,0 +1,14 @@
+#Python Lists and Arrays
+
+#Creating Lists
+print("Creating Lists")
+# Empty list
+x = []
+# List with initial values
+y = [1, 2, 3, 4, 5]
+# List with mixed types
+z = [1, "hello", 3.14, True]
+
+print(x)
+print(y)
+print(z)
