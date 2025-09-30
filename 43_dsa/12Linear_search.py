2c9f0df6d7ed2048d4b151ea3529dc46d64f1a70
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 04:29:54 +0600

    Python DSA - Linear search Linear Search to find the index of a value

--- 43_dsa/12Linear_search.py
+++ 43_dsa/12Linear_search.py
@@ -14,3 +14,21 @@ if 4 in mylist:
   print("Found!")
 else:
   print("Not found!")
+
+#But if you need to find the index of a value, you will need to implement a linear search
+print("\nLinear Search to find the index of a value")
+def linearSearch(arr, targetVal):
+  for i in range(len(arr)):
+    if arr[i] == targetVal:
+      return i
+  return -1
+
+mylist = [3, 7, 2, 9, 5, 1, 8, 4, 6]
+x = 4
+
+result = linearSearch(mylist, x)
+
+if result != -1:
+  print("Found at index", result)
+else:
+  print("Not found")
