7184107a2be68a9508b4326bbbed3b768ec2ea2b
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 04:44:46 +0600

    Python DSA - Selection Sort Improved Selection Sort algorithm

--- 43_dsa/15selection_sort.py
+++ 43_dsa/15selection_sort.py
@@ -23,3 +23,17 @@ for i in range(n-1):
   mylist.insert(i, min_value)
 
 print(mylist)
+
+#improved Selection Sort algorithm, including swapping values
+print("\nImproved Selection Sort algorithm")
+mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(mylist)
+for i in range(n):
+  min_index = i
+  for j in range(i+1, n):
+     if mylist[j] < mylist[min_index]:
+       min_index = j
+  mylist[i], mylist[min_index] = mylist[min_index], mylist[i]
+
+print(mylist)
