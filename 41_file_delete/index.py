dcf274d0265dc81eab9ee02da2d9d58642d4f5a7
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 18:45:09 +0600

    Python Delete File - Delete a File

---
+++ 41_file_delete/index.py
@@ -0,0 +1,7 @@
+#Python Delete File
+#Delete a File
+
+# To delete a file, you must import the OS module, and run its os.remove() function
+print("Delete a File:")
+import os
+os.remove("/home/abmn/Desktop/Python/41_file_delete/newfile.txt")
