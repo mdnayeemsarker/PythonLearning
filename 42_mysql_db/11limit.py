df1b475b834b87c64608a1937505285c621c5310
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-10-01 01:52:15 +0600

    Python MySql - Limit the result

---
+++ 42_mysql_db/11limit.py
@@ -0,0 +1,16 @@
+#Python MySQL Limit
+
+#Limit the Result
+print("Limit the Result")
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="abmn@!01",
+  database="mydatabase"
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM customers LIMIT 15")
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
