e86707be4bb0cc93dddb83a1b032e420c73cd219
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-30 21:27:30 +0600

    Python MySql - check if database exists is connected to mysql or not

--- 42_mysql_db/02mysqldb_create.py
+++ 42_mysql_db/02mysqldb_create.py
@@ -18,3 +18,15 @@ mycursor.execute("SHOW DATABASES")
 
 for x in mycursor:
   print(x)
+
+
+# check if database exists is connected to mysql or not
+print("\ncheck if database exists is connected to mysql or not")
+mydatabase = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="abmn@!01",
+  database="mydatabase"
+)
+
+print(mydatabase)
